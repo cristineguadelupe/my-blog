b84556b95be7b40497cb97f8e9a613326c533984
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-09 23:26:42 -0300

    Learning how to use gatsby Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 // TODO JS arrow function (learn more about it later!!!)
 const AboutPage = () => (
-    <h1>About Page</h1>
+    <>
+        <h1>About Page</h1>
+        <ul>
+        <li>
+            <Link to="/" activeStyle={ { color: 'red' } }>Home</Link>
+        </li>
+        <li>
+            <Link to="/about">About</Link>
+        </li>
+        </ul>
+    </>
 )
 
 export default AboutPage
