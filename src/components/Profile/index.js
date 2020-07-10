34a717731d76840b83109bcce4a9680002f23f50
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-09 23:35:50 -0300

    Profile initial component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Cristine Guadelupe</h1>
+        <h2>Elixir Alchemist</h2>
+        <p>My Elixir journey through real-time applications</p>
+    </div>
+)
+
+export default Profile
