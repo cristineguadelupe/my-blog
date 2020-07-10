e4cc456f18b7f6b8fa171b4eaed99b4adc121827
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-09 23:40:40 -0300

    Layout on the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import { Link } from 'gatsby'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 // TODO JS arrow function (learn more about it later!!!)
 const AboutPage = () => (
-    <>
+    <Layout>
+        <SEO title="About" />
         <h1>About Page</h1>
-        <ul>
-        <li>
-            <Link to="/" activeStyle={ { color: 'red' } }>Home</Link>
-        </li>
-        <li>
-            <Link to="/about">About</Link>
-        </li>
-        </ul>
-    </>
+        
+    </Layout>
 )
 
 export default AboutPage
