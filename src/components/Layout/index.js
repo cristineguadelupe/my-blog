1d6830d9e90ef9f52714d8c7c8332ff71501ea31
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-10 21:06:56 -0300

    Wrapper and Layout styled components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,24 +1,29 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Profile from "../Profile"
 
-const Layout = ({ children }) => {
+import styled from "styled-components"
 
+const LayoutWrapper = styled.section`
+  display: flex;
+`
+
+const LayoutMain = styled.main`
+  background: #16202c;
+  min-height: 100vh;
+  padding: 0 3.75rem 0 20rem;
+  width: 100%;
+`
+
+const Layout = ({ children }) => {
   return (
-    <>
+    <LayoutWrapper>
       <aside>
         <Profile />
       </aside>
-      <main>{children}</main>
-    </>
+      <LayoutMain>{children}</LayoutMain>
+    </LayoutWrapper>
   )
 }
 
