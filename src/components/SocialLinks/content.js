0156591e8005aa4b363117bb66b3cf0128082868
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-13 23:52:04 -0300

    Remove unused social icons

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -5,22 +5,22 @@ const links = [
     label: "Github",
     url: `https://github.com/${handle}`,
   },
-  {
-    label: "Twitter",
-    url: `https://github.com/${handle}`,
-  },
-  {
-    label: "Youtube",
-    url: `https://github.com/${handle}`,
-  },
-  {
-    label: "Unsplash",
-    url: "https://github.com/cristineguadelupe",
-  },
-  {
-    label: "Instagram",
-    url: `https://github.com/${handle}`,
-  },
+  // {
+  //   label: "Twitter",
+  //   url: `https://github.com/${handle}`,
+  // },
+  // {
+  //   label: "Youtube",
+  //   url: `https://github.com/${handle}`,
+  // },
+  // {
+  //   label: "Unsplash",
+  //   url: "https://github.com/cristineguadelupe",
+  // },
+  // {
+  //   label: "Instagram",
+  //   url: `https://github.com/${handle}`,
+  // },
 ]
 
 export default links
