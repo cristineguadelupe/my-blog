611e0993c83211d720794fb2c024c934b6e6dd37
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-14 01:05:55 -0300

    Night owl for prismjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,4 +7,5 @@
 // You can delete this file if you're not using it
 
 import "lazysizes"
-require("prismjs/themes/prism-tomorrow.css")
+import "prism-theme-night-owl";
+// require("prismjs/themes/prism-tomorrow.css")
