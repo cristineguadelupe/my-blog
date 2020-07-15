f250fb5f0be0b27060bfe11ee6fb638d1624c9fc
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-15 02:06:47 -0300

    About me - initial component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Web } from "@styled-icons/material-twotone/Web"
 
 import * as S from "./styled"
 
@@ -6,8 +7,24 @@ const About = () => (
   <S.AboutWrapper>
     <S.AboutHeader>
       <S.AboutTitle>Hi there! I'm Cristine</S.AboutTitle>
-      <S.AboutDescription>A Full Stack Web Developer who loves code, coffee and cats!</S.AboutDescription>
+      <S.AboutDescription>
+        A Full Stack Web Developer who loves code, coffee and cats!
+      </S.AboutDescription>
     </S.AboutHeader>
+    <S.MainContent>
+      {/* <S.AboutServicesSection>
+        <S.AboutServiceItem>
+          <S.AboutServiceInfo>
+          <S.AboutServiceIcon><Web /></S.AboutServiceIcon>
+            <h3>Real time web applications</h3>
+            <p>
+              Mauris neque libero, aliquet vel mollis nec, euismod sed tellus.
+              Mauris convallis dictum elit id volutpat.
+            </p>
+          </S.AboutServiceInfo>
+        </S.AboutServiceItem>
+      </S.AboutServicesSection> */}
+    </S.MainContent>
   </S.AboutWrapper>
 )
 
