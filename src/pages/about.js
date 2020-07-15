ab8a9a78a8a44a18b00159d6876d2dee1e96fa12
Cristine Guadelupe <cristineguadelupe@me.com>
2020-07-15 01:09:03 -0300

    About me - initial structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import About from "../components/About"
 
 // TODO JS arrow function (learn more about it later!!!)
 const AboutPage = () => (
     <Layout>
         <SEO title="About Me" />
-        <h1>About Page</h1>
+        <About />
         
     </Layout>
 )
